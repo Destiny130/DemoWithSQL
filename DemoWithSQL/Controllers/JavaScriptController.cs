a6beaeff436716dd6549859fdaf7c85fada102be
Destiny130 <destiny1307654641@hotmail.com>
2017-10-07 18:41:10 +0800

    Update

---
+++ DemoWithSQL/Controllers/JavaScriptController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWithSQL.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult JStest()
+        {
+            return View();
+        }
+    }
+}
