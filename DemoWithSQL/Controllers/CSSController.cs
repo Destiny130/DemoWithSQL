659bbf79e6f7c2481a5ce7c7140bf837f6f8be45
Destiny130 <destiny1307654641@hotmail.com>
2017-10-16 23:13:40 +0800

    add css demo

---
+++ DemoWithSQL/Controllers/CSSController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoWithSQL.Controllers
+{
+    public class CSSController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Fixed()
+        {
+            return View();
+        }
+    }
+}
