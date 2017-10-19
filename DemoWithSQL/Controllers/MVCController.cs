c9364ecc736b24c90b976ce5bc1c5b02857d0b60
Destiny130 <destiny1307654641@hotmail.com>
2017-10-20 00:12:42 +0800

    add demo

--- DemoWithSQL/Controllers/MVCController.cs
+++ DemoWithSQL/Controllers/MVCController.cs
@@ -4,20 +4,41 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using DemoWithSQL.Models;
+using System.Data.Objects;
+using System.Text;
+using System.IO;
+//using System.Data.Entity.Core.Objects;
+//using System.Management;
 
 namespace DemoWithSQL.Controllers
 {
     public class MVCController : Controller
     {
+        DemoSQLEntities db = new DemoSQLEntities();
+
         public ActionResult Index()
         {
+            var haha = db.SysAuths.Where(s => s.TypeKey > 0 && s.ControllerName.Contains("e")).OrderByDescending(r => r.TypeKey).Select(s => s.ID);
+            int i = 0;
+            foreach (var temp in haha)
+            {
+                i++;
+            }
+            ViewBag.test = i.ToString();
             return View();
         }
 
-        DemoSQLEntities db = new DemoSQLEntities();
         public ActionResult MVC_SetAuth()
         {
-            var laterUse = db.SysAuths.Where(s => s.TypeKey == 1);
+            IQueryable<SysAuth> laterUse = db.SysAuths.Where(s => s.TypeKey == 1);
+            //ObjectQuery<SysAuth> sysauth = db.SysAuths;
+            //db.CommandTimeOut = 50000;
+            //var temp = MyExtensions.ToTraceString<SysAuth>(laterUse);
+
+            //db.Log = new DebugTextWriter();
+            //var temp = ((System.Data.Entity.Core.Objects.ObjectQuery)laterUse).ToTraceString();
+            //ViewBag.temp = temp;
+
             var controllerNameList = laterUse.ToList().Select(s => s.ControllerName).Distinct();
             Dictionary<string, List<SelectListItem>> shows = new Dictionary<string, List<SelectListItem>>();
             foreach (string controllerName in controllerNameList)
@@ -75,13 +96,13 @@ namespace DemoWithSQL.Controllers
             return View();
         }
 
-        public ActionResult GetParams(string str/*, int id, string[] strs, int[] ids, Fruit fruit, Fruit[] fruits*/)
+        public ActionResult GetParams(string str, int id/*, string[] strs, int[] ids, Fruit fruit, Fruit[] fruits*/)
         {
             string ret = "show: ";
             try
             {
                 ret += str + ",&nbsp;";
-                //ret += id.ToString() + "<br />";
+                ret += id.ToString() + "<br />";
                 //foreach (string s in strs)
                 //{
                 //    ret += s + ",&nbsp;";
@@ -104,6 +125,67 @@ namespace DemoWithSQL.Controllers
                 ret += ex.Message;
             }
             return Content(ret);
+        }
+
+        public ActionResult GetParams_jQuery(string str, int id, string[] strs, int[] ids, Fruit fruit, Fruit[] fruits)
+        {
+            string ret = "";
+            try
+            {
+                ret += str + ",&nbsp;";
+                ret += id.ToString() + "<br />";
+                foreach (string s in strs)
+                {
+                    ret += s + ",&nbsp;";
+                }
+                ret += "<br />";
+                foreach (int i in ids)
+                {
+                    ret += i.ToString() + ",&nbsp;";
+                }
+                ret += "<br />";
+                ret += fruit.Name + "<br />";
+                foreach (Fruit f in fruits)
+                {
+                    ret += f.Name + ",&nbsp;";
+                }
+                ret += "<br />";
+            }
+            catch (Exception ex)
+            {
+                ret += ex.Message;
+            }
+            return Content(ret);
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static string ToTraceString<T>(this IQueryable<T> t)
+        {
+            string sql = "";
+            ObjectQuery<T> oqt = t as ObjectQuery<T>;
+            if (oqt != null)
+                sql = oqt.ToTraceString();
+            return sql;
+        }
+    }
+
+    class DebugTextWriter : System.IO.TextWriter
+    {
+        public override void Write(char[] buffer, int index, int count)
+        {
+            System.Diagnostics.Debug.Write(new String(buffer, index, count));
+        }
+
+        public override void Write(string value)
+        {
+            System.Diagnostics.Debug.Write(value);
+        }
+
+        public override Encoding Encoding
+        {
+            get { return System.Text.Encoding.Default; }
         }
     }
 
